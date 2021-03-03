cb8df3c52f6b61c3a336072d5b3ab1f85ed8fc65
Edo Skaljo <eskaljo1@etf.unsa.ba>
2021-03-03 11:49:47 +0100

    Pocetni commit

---
+++ UndergroundProject/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float sensitivityX = 8F;
+    public float sensitivityY = 8F;
+    public float minimumY = -90.0f;
+    public float maximumY = 90.0f;
+    float rotationY = 0.0f;
+    float rotationX = 180.0f;
+
+    //Update rotation of camera
+    void Update()
+    {
+            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+    }
+}
