72ce7b981c8d742cbddbd681b3b85a803b1b43ce
Edo Skaljo <eskaljo1@etf.unsa.ba>
2021-03-05 14:50:55 +0100

    Implementirana navigacija u S1.1

---
+++ UndergroundProject/Assets/Scripts/ArrowRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowRotation : MonoBehaviour
+{
+    public int rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime, 0, 0);
+    }
+}
--- UndergroundProject/Assets/Scripts/MainMenuNavigation.cs
+++ UndergroundProject/Assets/Scripts/MainMenuNavigation.cs
@@ -10,6 +10,21 @@ public class MainMenuNavigation : MonoBehaviour
     //Open scene
     void OpenScene()
     {
+        //Return to menu scene
+        if(sceneNumber == "0")
+        {
+            SceneManager.LoadScene("MenuScene");
+            return;
+        }
+
+        //Moving from scene S1.1 to S1.2
+        if(sceneNumber == "6")
+        {
+            SceneManager.LoadScene("S1.2");
+            return;
+        }
+
+        //Rest of the scenes
         string s = "S" + sceneNumber;
         if (sceneNumber == "1")
             s = s + ".1";
