0ff365e0c25b3f1d7db20bc9bf356d692837537c
Edo Skaljo <eskaljo1@etf.unsa.ba>
2021-03-12 11:57:44 +0100

    dodan help panel

---
+++ UndergroundProject/Assets/Scripts/HelpPanel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpPanel : MonoBehaviour
+{
+    public void ExitHelp()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void OpenHelp()
+    {
+        gameObject.SetActive(true);
+    }
+}
