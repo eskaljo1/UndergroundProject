f05a3136d5b2a50ea09555fcb95107dd378cc6d5
Edo Skaljo <eskaljo1@etf.unsa.ba>
2021-03-19 12:32:44 +0100

    dodan fade in i fade out

--- UndergroundProject/Assets/Scripts/PlayLinkVideo.cs
+++ UndergroundProject/Assets/Scripts/PlayLinkVideo.cs
@@ -7,11 +7,26 @@ public class PlayLinkVideo : MonoBehaviour
 {
     public string nameOfVideo;
     private VideoPlayer videoPlayer;
-    
+
     void Awake()
     {
-        videoPlayer = GetComponent<VideoPlayer>();
-        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, nameOfVideo + ".mp4");
+        if (videoPlayer == null)
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+            videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, nameOfVideo + ".mp4");
+            videoPlayer.prepareCompleted += (source) =>
+            {
+                source.Play();
+            };
+            videoPlayer.Prepare();
+        }
     }
 
+    void OnEnable()
+    {
+        if (videoPlayer.isPrepared)
+            videoPlayer.Play();
+        else
+            videoPlayer.Prepare();
+    }
 }
